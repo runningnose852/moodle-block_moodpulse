bd2c27111da44ee70cfbc96e43423bcbdb1b6a23
runningnose852 <runningcalligraphy@gmail.com>
2026-03-20 10:47:57 +0000

    Update block_moodpulse.php

--- lang/en/block_moodpulse.php
+++ lang/en/block_moodpulse.php
@@ -1,43 +1,32 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-{{!
-    This file is part of Moodle - http://moodle.org/
-
-    Moodle is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Moodle is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-}}
-{{!
-    @template block_moodpulse/moodpulse
-
-    Mood Pulse block template.
-
-    Context variables required for this template:
-    * question - The question text
-    * moods - Array of mood options
-
-    @package   block_moodpulse
-    @copyright 2026 Wan Ling
-    @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
-}}
-
-<div class="moodpulse">
-    <p>{{question}}</p>
-</div>
+/**
+ * Language strings for the Mood Pulse block.
+ *
+ * @package   block_moodpulse
+ * @copyright 2026 Wan Ling
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 $string['pluginname'] = 'Mood Pulse';
 $string['moodpulse:addinstance'] = 'Add a new Mood Pulse block';
 $string['moodpulse:myaddinstance'] = 'Add a new Mood Pulse block to the Dashboard';
 $string['moodpulse:vote'] = 'Submit a mood vote';
+$string['privacy:metadata'] = 'The Mood Pulse block does not store any personal data.';
 
 $string['prompt'] = 'How are you feeling today?';
 $string['question'] = 'How are you feeling today?';
@@ -58,5 +47,3 @@ $string['resultsheading'] = 'Class mood so far';
 $string['novotes'] = 'No votes yet.';
 $string['checkedintoday'] = '{$a} students checked in today';
 $string['editinghint'] = 'Teachers can customise the question in block settings.';
-$string['privacy:metadata'] = 'The Mood Pulse block does not store any personal data.';
-
