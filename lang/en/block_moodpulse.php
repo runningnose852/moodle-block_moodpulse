0507461ed4125b9a320b49a29f46196608c13c6d
runningnose852 <runningcalligraphy@gmail.com>
2026-03-20 10:14:55 +0000

    Update block_moodpulse.php

--- lang/en/block_moodpulse.php
+++ lang/en/block_moodpulse.php
@@ -1,26 +1,38 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Language strings for the Mood Pulse block.
- *
- * @package   block_moodpulse
- * @copyright 2026 Wan Ling
- * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
+{{!
+    This file is part of Moodle - http://moodle.org/
+
+    Moodle is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Moodle is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+}}
+{{!
+    @template block_moodpulse/moodpulse
+
+    Mood Pulse block template.
+
+    Context variables required for this template:
+    * question - The question text
+    * moods - Array of mood options
+
+    @package   block_moodpulse
+    @copyright 2026 Wan Ling
+    @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+}}
+
+<div class="moodpulse">
+    <p>{{question}}</p>
+</div>
 
 $string['pluginname'] = 'Mood Pulse';
 $string['moodpulse:addinstance'] = 'Add a new Mood Pulse block';
